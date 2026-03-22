c3cd08432cc6845f4d22a7cc3e81d4642524a389
SumitChatterjeee <subrothochatt123@gmail.com>
2026-03-23 00:42:50 +0530

    Packages Created

---
+++ src/test/java/com/sumit/StackGen/StackGenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumit.StackGen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StackGenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
