4957bacdb607d56f88ac92f55a88c8a4473f5b61
SumitChatterjeee <subrothochatt123@gmail.com>
2026-04-11 01:12:06 +0530

    Security Added

---
+++ src/main/java/com/sumit/StackGen/Security/JwtUserPrincipal.java
@@ -0,0 +1,10 @@
+package com.sumit.StackGen.Security;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.List;
+
+public record JwtUserPrincipal(Long userId,
+                               String username,
+                               List<GrantedAuthority>authorities) {
+}
